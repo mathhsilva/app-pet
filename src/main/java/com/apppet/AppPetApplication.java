bba65a44dfd85e5db70ffd626ea4f443f9ced2df
Matheus Cordeiro <matheus.cordeiro@rpinfo.com.br>
2024-09-10 13:59:58 -0300

    Configuração do Projeto juntamente com o Application.properties

---
+++ src/main/java/com/apppet/AppPetApplication.java
@@ -0,0 +1,13 @@
+package com.apppet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppPetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/apppet/AppPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apppet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppPetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
