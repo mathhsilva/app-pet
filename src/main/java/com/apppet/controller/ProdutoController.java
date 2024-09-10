a21630a4ca843afec10129cb72dac0096854547f
Matheus Cordeiro <matheus.cordeiro@rpinfo.com.br>
2024-09-10 14:41:54 -0300

    Ajustes no Controller (Adicionado o Post do produto e utilizando o @Service ao invés do Repository)

--- src/main/java/com/apppet/controller/ProdutoController.java
+++ src/main/java/com/apppet/controller/ProdutoController.java
@@ -2,7 +2,11 @@ package com.apppet.controller;
 
 import com.apppet.entity.Produto;
 import com.apppet.repository.ProdutoRepository;
+import com.apppet.service.ProdutoService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -11,13 +15,21 @@ import java.util.List;
 public class ProdutoController {
 
     private final ProdutoRepository produtoRepository;
+    private final ProdutoService produtoService;
 
-    public ProdutoController(ProdutoRepository produtoRepository) {
+    public ProdutoController(ProdutoRepository produtoRepository, ProdutoService produtoService) {
         this.produtoRepository = produtoRepository;
+        this.produtoService = produtoService;
     }
 
     @GetMapping("/produtos")
     public List<Produto> listaTodos() {
-        return produtoRepository.findAll();
+        return produtoService.listarTodos();
+    }
+
+    @PostMapping("/produtos")
+    public ResponseEntity<Produto> criarProduto(@RequestBody Produto produto) {
+        Produto novoProduto = produtoRepository.save(produto);
+        return ResponseEntity.ok(novoProduto);
     }
 }
